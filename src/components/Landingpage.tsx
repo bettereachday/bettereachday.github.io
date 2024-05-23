2fb5754ddb88ccac9b467fbd1193dacc086153e9
= <mail@jesperbylund.com>
2024-05-23 14:37:05 +0200

    Stackable animated cards, with some wonk

--- src/components/Landingpage.tsx
+++ src/components/Landingpage.tsx
@@ -1,7 +1,48 @@
 // import { motion, useViewportScroll, useTransform } from "framer-motion";
 import Logo from "../components/Logo";
 import CalEmbed from "../components/CalEmbed";
+import { motion, useScroll, useTransform, useMotionValueEvent } from "framer-motion"
+import { useRef } from "react";
+
+function LandingPageCard(props: any): JSX.Element {
+  const {containerRef, stack} = props;
+  const [stackNumber, stackHeight] = stack || [];
+  const minScale = (100 - (10 - stackNumber)) * 0.01;
+  
+
+  const ref = useRef(null);
+
+  const { scrollYProgress: containerScrollProgress } = useScroll({ container: containerRef });
+  const { scrollYProgress } = useScroll({ target: ref, offset: ["start end", "5vh 5vh"] });
+  
+  const scale = useTransform(scrollYProgress, [0, 0.2, 0.9, 0.95], [1, 1, 1, minScale], { clamp: true });
+  const opacity = useTransform(scrollYProgress, [0, 0.1, 0.95, 1], [0, 1, 1, 1], { clamp: true });
+  // const translate = useTransform(scrollYProgress, [0, 0.95, 1], [0, -10, -10], { clamp: true });
+  const translate = 0;
+
+  return (<motion.div 
+      ref={ref}
+      //@ts-ignore
+      style={{ 
+        scaleX: scale, 
+        scaleY: scale, 
+        opacity: opacity,
+        translateY: (stackNumber * 10) -10,
+        transformStyle: 'preserve-3d'
+      }} 
+      className="card"
+    >{props.children}</motion.div>)
+}
+
 export default function Landingpage({}) {
+
+  const clarityRef = useRef(null);
+  const analyseRef = useRef(null);
+  const automateRef = useRef(null);
+
+  // useMotionValueEvent(articleOneScrollProgress, "change", (latest) => {
+  //   console.log(articleOneScrollProgress.current, articleOneScale.current);
+  // })
   // const { scrollYProgress } = useViewportScroll();
   // const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
 
@@ -16,23 +57,23 @@ export default function Landingpage({}) {
           <div className="bg-image"></div>
         </div>
       </header>
-      <section>
+      <section ref={clarityRef}>
         <div className="wrapper">
           <div className="header">
             <h2>Clarify</h2>
           </div>
           <div className="body">
-            <article>
+            <LandingPageCard container={clarityRef} stack={[1,2]}>
               <div className="title">1. Workshop</div>
               Free customer journey workshop with a senior product designer or
               developer
-            </article>
-            <article>
-            <div className="title">2. Clarity</div>
+            </LandingPageCard>
+            <LandingPageCard container={clarityRef} stack={[2,2]}>
+              <div className="title">2. Clarity</div>
               After this workshop you will have greater clarity on how your work
               impacts your customers. And will have several things you can
               automate.
-            </article>
+            </LandingPageCard>
           </div>
         </div>
       </section>
@@ -42,20 +83,20 @@ export default function Landingpage({}) {
             <h2>Analyse</h2>
           </div>
           <div className="body">
-            <article>
-            <div className="title">3. Gather</div>
+            <LandingPageCard container={analyseRef} stack={[1,3]}>
+              <div className="title">3. Gather</div>
               Don’t have the right data? We help you set up collection.
-            </article>
-            <article>
+            </LandingPageCard>
+            <LandingPageCard container={analyseRef} stack={[2,3]}>
             <div className="title">4. Use</div>
               Have the right data but it’s not used? We help you set up the
               process.{" "}
-            </article>
-            <article>
+            </LandingPageCard>
+            <LandingPageCard container={analyseRef} stack={[3,3]}>
             <div className="title">5. Clarity</div>
               After this step will have better insights into customer processes
               and how they impact your bottom line.
-            </article>
+            </LandingPageCard>
           </div>
         </div>
       </section>
@@ -66,26 +107,26 @@ export default function Landingpage({}) {
             <h2>Automate</h2>
           </div>
           <div className="body">
-            <article>
-              <div className="card">
+            <LandingPageCard container={automateRef} stack={[1,3]}>
+              <div className="card-bg">
               <div className="title">6. Choose</div>
                 You pick a process, or step in a process, we build the
                 automation.
               </div>
-            </article>
-            <article>
-              <div className="card">
+            </LandingPageCard>
+            <LandingPageCard container={automateRef} stack={[2,3]}>
+              <div className="card-bg">
               <div className="title">7. Simplify</div>
                 Does it need a human in the loop? Or an AI? We’ll help you set
                 that up.
               </div>
-            </article>
-            <article>
-              <div className="card">
+            </LandingPageCard>
+            <LandingPageCard container={automateRef} stack={[3,3]}>
+              <div className="card-bg">
               <div className="title">8. Operationalise</div>
                 Need to self host? We’ll help you set up on prem.
               </div>
-            </article>
+            </LandingPageCard>
           </div>
         </div>
       </section>
