31232ccabf49223eaf74dbfdbe16dffae4f70a27
= <mail@jesperbylund.com>
2024-05-28 10:40:49 +0200

    Most improvements discussed

--- src/components/Landingpage.tsx
+++ src/components/Landingpage.tsx
@@ -7,15 +7,14 @@ import { useRef } from "react";
 function LandingPageCard(props: any): JSX.Element {
   const {containerRef, stack} = props;
   const [stackNumber, stackHeight] = stack || [];
-  const minScale = (100 - (10 - stackNumber)) * 0.01;
-  
+  const minScale = (100 - (5 - stackNumber)) * 0.01;
 
   const ref = useRef(null);
 
   const { scrollYProgress: containerScrollProgress } = useScroll({ container: containerRef });
   const { scrollYProgress } = useScroll({ target: ref, offset: ["start end", "start 22vh"] });
   
-  const scale = useTransform(scrollYProgress, [0, 0.9, 0.95], [1, 1, minScale], { clamp: true });
+  const scale = useTransform(scrollYProgress, [0, 0.97, 0.99], [1, 1, minScale], { clamp: true });
   const opacity = useTransform(scrollYProgress, [0, 0.1], [0, 1], { clamp: true });
   // const translate = useTransform(scrollYProgress, [0, 0.95, 1], [0, -10, -10], { clamp: true });
   const translate = 0;
@@ -61,7 +60,7 @@ export default function Landingpage({}) {
               developer
             </LandingPageCard>
             <LandingPageCard container={clarityRef} stack={[2,2]}>
-              <div className="title">2. Clarity</div>
+              <div className="title">2. Process map</div>
               After this workshop you will have greater clarity on how your work
               impacts your customers. And will have several things you can
               automate.
@@ -77,17 +76,15 @@ export default function Landingpage({}) {
           <div className="body">
             <LandingPageCard container={analyseRef} stack={[1,3]}>
               <div className="title">3. Gather</div>
-              Don’t have the right data? We help you set up collection.
+              Don&apos;t have the right data? We help you set up collection.
             </LandingPageCard>
             <LandingPageCard container={analyseRef} stack={[2,3]}>
             <div className="title">4. Use</div>
-              Have the right data but it’s not used? We help you set up the
-              process.{" "}
+              We help you set up your data process so you have actionable data.
             </LandingPageCard>
             <LandingPageCard container={analyseRef} stack={[3,3]}>
-            <div className="title">5. Clarity</div>
-              After this step will have better insights into customer processes
-              and how they impact your bottom line.
+            <div className="title">5. Data funnel</div>
+            After this step you will have better insights into all targeted business processes and how they impact your bottom line
             </LandingPageCard>
           </div>
         </div>
@@ -99,26 +96,22 @@ export default function Landingpage({}) {
             <h2>Automate</h2>
           </div>
           <div className="body">
-            <LandingPageCard container={automateRef} stack={[1,3]}>
-              <div className="card-bg">
+            <article className="card">
+              <div className="card-bg"></div>
               <div className="title">6. Choose</div>
-                You pick a process, or step in a process, we build the
-                automation.
-              </div>
-            </LandingPageCard>
-            <LandingPageCard container={automateRef} stack={[2,3]}>
-              <div className="card-bg">
+                Pick a manual process or step in that process, we will build automation
+
+            </article>
+            <article className="card">
+              <div className="card-bg"></div>
               <div className="title">7. Simplify</div>
-                Does it need a human in the loop? Or an AI? We’ll help you set
-                that up.
-              </div>
-            </LandingPageCard>
-            <LandingPageCard container={automateRef} stack={[3,3]}>
-              <div className="card-bg">
+                Does it need a human in the loop? Or an autonomous AI? We will help you set up that pipeline.
+            </article>
+            <article className="card">
+              <div className="card-bg"></div>
               <div className="title">8. Operationalise</div>
-                Need to self host? We’ll help you set up on prem.
-              </div>
-            </LandingPageCard>
+                Need to self host? We&apos;ll help you set up on prem.
+            </article>
           </div>
         </div>
       </section>
